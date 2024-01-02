ece08b7937518dc47fe9ff92a68911cf75fd54f5
KaterinaMysl <myslivchik382@gmail.com>
2024-01-02 20:10:07 +0300

    feat: add design

--- src/Layouts/Header/Header.tsx
+++ src/Layouts/Header/Header.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { HEADER } from '../../utils/constants';
@@ -6,8 +7,22 @@ import Navigate from '../../pages/Welcome/Navigate';
 import styles from './Header.module.css';
 
 const Header = () => {
+  const [isSticky, setSticky] = useState(false);
+
+  const handleScroll = () => {
+    setSticky(window.scrollY > 0);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${isSticky ? styles.sticky : ''}`}>
       <Link to={HEADER.link} className={styles.logo}>
         <h2 className={styles.title}>{HEADER.title}</h2>
       </Link>
