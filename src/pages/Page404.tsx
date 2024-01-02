f4c0989b73779f8201ca7fc63df3d529fda62587
KaterinaMysl <myslivchik382@gmail.com>
2024-01-02 18:48:52 +0300

    feat: add design to page for 404

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,5 +1,17 @@
+import styles from './Page404.module.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Page404 = () => {
-  return <div>404 Page</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>404 Page Not Found</h1>
+      <p className={styles.description}>
+        The page you are looking for might be in another universe.{<br />}
+        <Link to="/">Go back to the home page.</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Page404;
